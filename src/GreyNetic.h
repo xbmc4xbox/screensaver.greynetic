a97236a869eb4cfae82f5ab6a0797d27153aa992
antonic901 <antonicnikola6@gmail.com>
2025-05-07 00:57:57 +0200

    added: source code of greynetic screensaver

---
+++ src/GreyNetic.h
@@ -0,0 +1,31 @@
+// include file for screensaver template
+
+#include <xtl.h>
+
+extern "C" void d3dSetTextureStageState( int x, DWORD dwY, DWORD dwZ);
+extern "C" void d3dSetRenderState(DWORD dwY, DWORD dwZ);
+extern "C" void d3dGetRenderState(DWORD dwY, DWORD* dwZ);
+
+void LoadSettings();
+void SetDefaults();
+void CreateArrays();
+
+LPDIRECT3DDEVICE8 m_pd3dDevice;
+
+#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZRHW|D3DFVF_DIFFUSE)
+
+
+LPDIRECT3DVERTEXBUFFER8 g_pVertexBuffer = NULL; // Vertices Buffer
+
+static  char m_szScrName[1024];
+int	m_iWidth;
+int m_iHeight;
+
+void hsv_to_rgb(double hue, double saturation, double value, double *red, double *green, double *blue);
+void DrawRectangle(int x, int y, int w, int h, D3DCOLOR dwColour);
+
+struct MYCUSTOMVERTEX
+{
+	FLOAT x, y, z, rhw; // The transformed position for the vertex.
+    DWORD colour; // The vertex colour.
+};
